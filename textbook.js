62c81226c7303f85e12d668e9cf6f547c652048e
najla <najlavahdani123@gmail.com>
2023-09-21 11:52:50 +0330

    textbook page added

---
+++ textbook.js
@@ -0,0 +1,11 @@
+const part1Btn = document.querySelector('part-1');
+const part1Content = document.querySelector('part-1-content');
+
+console.log(part1Btn);
+
+// const hideFunc = function () {
+//     console.log("najla");
+// }
+// part1Btn.addEventListener('click' , hideFunc)
+    
+    // part1Content.classList.toggle('hidden-parts');
